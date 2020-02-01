29870d9de1d2a6dd8e72b0c00f38d9c6646afa56
wbrowne <wbrowne@tcd.ie>
2020-02-01 11:33:01 +0100

    Part 1 complete

---
+++ 1-quiz/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	CsvFilename = "problems.csv"
+	MaxProblems = 100
+)
+
+func (p Problem) String() string {
+	return fmt.Sprintf("Problem: %s? | Answer: %s", p.question, p.answer)
+}
+
+type Problem struct {
+	question string
+	answer   string
+}
+
+func main() {
+	problems, err := getProblemsFromCsv(CsvFilename)
+
+	if len(problems) > MaxProblems {
+		log.Printf("That's too many problems! Try less than %d\n", MaxProblems)
+		return
+	}
+
+	if err != nil {
+		log.Println("Error:", err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	var numCorrect = 0
+	for _, problem := range problems {
+		log.Print(problem.question)
+
+		userInput := getUserInput(reader)
+		userInput = strings.TrimRight(userInput, "\n")
+		if userInput == problem.answer {
+			numCorrect++
+		}
+	}
+
+	log.Printf("You answered %d/%d correctly", numCorrect, len(problems))
+}
+
+func getUserInput(reader *bufio.Reader) string {
+	for {
+		result, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return result
+	}
+}
+
+func getProblemsFromCsv(filename string) ([]Problem, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	rows, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var problems []Problem
+	for _, row := range rows {
+		problems = append(problems, Problem{question: row[0], answer: row[1]})
+	}
+
+	return problems, nil
+}
