1dcf5b6cc37105dad139aee10603b2091c49e4db
wbrowne <wbrowne@tcd.ie>
2020-02-01 14:58:13 +0100

    Part 2 complete

--- 1-quiz/main.go
+++ 1-quiz/main.go
@@ -8,11 +8,13 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 const (
-	CsvFilename = "problems.csv"
-	MaxProblems = 100
+	CsvFilename      = "problems.csv"
+	MaxProblems      = 100
+	TimeoutInSeconds = 30
 )
 
 func (p Problem) String() string {
@@ -25,42 +27,54 @@ type Problem struct {
 }
 
 func main() {
-	problemsFilePtr := flag.String("f", "problems.csv", "a CSV file of problems")
+	problemsFilePtr := flag.String("f", CsvFilename, "a CSV file of problems")
+	timeoutPtr := flag.Int("t", TimeoutInSeconds, "a problem timeout value")
 	flag.Parse()
+
 	problems, err := getProblemsFromCsv(*problemsFilePtr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if len(problems) > MaxProblems {
 		log.Printf("That's too many problems! Try less than %d\n", MaxProblems)
 		return
 	}
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	log.Println("Hit enter to start")
 	reader := bufio.NewReader(os.Stdin)
+	reader.ReadString('\n')
+
 	var numCorrect = 0
 	for _, problem := range problems {
-		log.Print(problem.question)
+		log.Println(problem.question)
 
-		userInput := getUserInput(reader)
-		if userInput == problem.answer {
-			numCorrect++
+		input := make(chan string)
+		go getUserInput(input)
+
+		select {
+		case userInput, _ := <-input:
+			if userInput == problem.answer {
+				numCorrect++
+			}
+		case <-time.After(time.Duration(*timeoutPtr) * time.Second):
+			log.Println("Not quick enough!")
+			return
 		}
 	}
 
 	log.Printf("You answered %d/%d correctly", numCorrect, len(problems))
 }
 
-func getUserInput(reader *bufio.Reader) string {
-	for {
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return strings.TrimRight(input, "\n")
+func getUserInput(input chan string) {
+	reader := bufio.NewReader(os.Stdin)
+	result, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	input <- strings.TrimRight(result, "\n")
 }
 
 func getProblemsFromCsv(filename string) ([]Problem, error) {
